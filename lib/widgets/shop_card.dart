62020065b8ccddf825b9156618597761a2fb8320
venuzzi <rifqipratama2004@gmail.com>
2023-11-08 15:47:29 +0700

    Tugas 7 selesai

---
+++ lib/widgets/shop_card.dart
@@ -0,0 +1,9 @@
+import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:flutter/material.dart';
+
+class ShopItem {
+  final String name;
+  final IconData icon;
+
+  ShopItem(this.name, this.icon);
+}
