6f9be461a467b7749ebe395584b347332f90098f
venuzzi <rifqipratama2004@gmail.com>
2023-11-15 16:20:48 +0700

    Tutor 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_list/screens/login.dart';
 import 'package:shopping_list/screens/menu.dart';
 
 void main() {
@@ -11,22 +14,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        primarySwatch: Colors.blue,
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginPage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
+  //   return MaterialApp(
+  //     title: 'Flutter Demo',
+  //     theme: ThemeData(
+  //       // This is the theme of your application.
+  //       //
+  //       // Try running your application with "flutter run". You'll see the
+  //       // application has a blue toolbar. Then, without quitting the app, try
+  //       // changing the primarySwatch below to Colors.green and then invoke
+  //       // "hot reload" (press "r" in the console where you ran "flutter run",
+  //       // or simply save your changes to "hot reload" in a Flutter IDE).
+  //       // Notice that the counter didn't reset back to zero; the application
+  //       // is not restarted.
+  //       colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     home: MyHomePage(),
+  //   );
+  // }
